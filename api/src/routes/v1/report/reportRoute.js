1c3c9366b76dd92f344ef53e3642449d7e0072d9
HaAlpaca <dangminhhaworkspace@gmail.com>
2024-10-11 22:43:19 +0700

    add report api for dashboard

--- api/src/routes/v1/report/reportRoute.js
+++ api/src/routes/v1/report/reportRoute.js
@@ -1,6 +1,16 @@
 import express from "express";
-import { sumItem } from "../../../controllers/v1/report/reportController.js";
+import {
+  sumBill,
+  sumBillForChart,
+  sumItem,
+  sumProductfromProvider,
+  tableStatusOccupied,
+} from "../../../controllers/v1/report/reportController.js";
 const Router = express.Router();
 
 Router.get("/sumitem", sumItem);
+Router.get("/sumbill", sumBill);
+Router.get("/sumproductfromprovider", sumProductfromProvider);
+Router.get("/sumbillforchart", sumBillForChart);
+Router.get("/tablestatusoccupied", tableStatusOccupied);
 export const reportRoute = Router;
