101e7fb583bce3551e9fa249dfdee3df11a3cb23
HaAlpaca <dangminhhaworkspace@gmail.com>
2024-10-25 16:31:12 +0700

    fix add product item join table

--- api/src/controllers/v1/joinTable/productitemController.js
+++ api/src/controllers/v1/joinTable/productitemController.js
@@ -11,7 +11,23 @@ const addProductItem = async (req, res, next) => {
   try {
     // Kiểm tra nếu không có dữ liệu
     if (!Array.isArray(items) || items.length === 0) {
-      return res.status(400).json("Missing type array in req.body");
+      return res
+        .status(400)
+        .json({ message: "Missing type array in req.body" });
+    }
+
+    // Kiểm tra xem trong mảng có phần tử nào trùng lặp cặp products_id và items_id không
+    const uniquePairs = new Set();
+    for (const { products_id, items_id } of items) {
+      const pairKey = `${products_id}-${items_id}`; // Tạo một key duy nhất cho mỗi cặp
+
+      if (uniquePairs.has(pairKey)) {
+        return res.status(400).json({
+          message: `Duplicate pair of products_id ${products_id} and items_id ${items_id} in the array`,
+        });
+      }
+
+      uniquePairs.add(pairKey);
     }
 
     // Kiểm tra tất cả các products và items trước khi tiến hành thêm
@@ -20,7 +36,7 @@ const addProductItem = async (req, res, next) => {
       if (!products_id || !items_id || !quantity) {
         return res
           .status(400)
-          .json("Missing products_id or items_id or quantity");
+          .json({ message: "Missing products_id or items_id or quantity" });
       }
 
       // Kiểm tra products_id có tồn tại không
@@ -31,7 +47,7 @@ const addProductItem = async (req, res, next) => {
       if (productExists.rowCount === 0) {
         return res
           .status(400)
-          .json(`Products_id ${products_id} does not exist`);
+          .json({ message: `Products_id ${products_id} does not exist` });
       }
 
       // Kiểm tra items_id có tồn tại không
@@ -40,7 +56,20 @@ const addProductItem = async (req, res, next) => {
         [items_id]
       );
       if (itemExists.rowCount === 0) {
-        return res.status(400).json(`Items_id ${items_id} does not exist`);
+        return res
+          .status(400)
+          .json({ message: `Items_id ${items_id} does not exist` });
+      }
+
+      // Kiểm tra xem cặp products_id và items_id đã tồn tại trong Products_Items chưa
+      const existingPair = await pool.query(
+        `SELECT 1 FROM Products_Items WHERE products_id = $1 AND items_id = $2`,
+        [products_id, items_id]
+      );
+      if (existingPair.rowCount > 0) {
+        return res.status(400).json({
+          message: `The pair of products_id ${products_id} and items_id ${items_id} already exists in the database`,
+        });
       }
     }
 
