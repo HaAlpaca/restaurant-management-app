d4541c0702f2f69b43a591f15b539b2d0ddc3e1f
HaAlpaca <dangminhhaworkspace@gmail.com>
2024-11-06 10:12:06 +0700

    add auth, not protectRoute

---
+++ api/src/controllers/v1/auth/authController.js
@@ -0,0 +1,113 @@
+import { StatusCodes } from "http-status-codes";
+import { env } from "../../../config/environment.js";
+import { JwtProvider } from "../../../providers/JwtProvider.js";
+import { pool } from "../../../config/db.js";
+import bcrypt from "bcrypt";
+const login = async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+
+    // Kiểm tra email hoặc username từ bảng Staff
+    const query = `
+        SELECT * FROM Staff 
+        WHERE (email = $1 OR username = $2) 
+        LIMIT 1
+      `;
+    const values = [email, username];
+    const { rows } = await pool.query(query, values);
+    const user = rows[0];
+    // Nếu không tìm thấy người dùng
+    if (!user) {
+      return res
+        .status(StatusCodes.FORBIDDEN)
+        .json({ message: "Your email/username or password is incorrect" });
+    }
+    // if (!user) {
+    //   return res
+    //     .status(StatusCodes.FORBIDDEN)
+    //     .json({ message: "Your email/username or password is incorrect!" });
+    // }
+
+    // Kiểm tra password bằng cách so sánh password_hash
+    const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+    if (!isPasswordValid) {
+      return res
+        .status(StatusCodes.FORBIDDEN)
+        .json({ message: "Your email/username or password is incorrect!" });
+    }
+
+    // Tạo token và trả về cho phía Client nếu thông tin tài khoản đúng
+    const userInfo = {
+      id: user.staff_id,
+      email: user.email,
+      username: user.username,
+      role: user.role,
+    };
+
+    // Tạo accessToken và refreshToken
+    const accessToken = await JwtProvider.generateToken(
+      userInfo,
+      env.ACCESS_TOKEN_SECRET_SIGNATURE,
+      "1h"
+    );
+    const refreshToken = await JwtProvider.generateToken(
+      userInfo,
+      env.REFRESH_TOKEN_SECRET_SIGNATURE,
+      "14 days"
+    );
+
+    // Trả về token cho FE lưu vào localStorage
+    res.status(StatusCodes.OK).json({
+      ...userInfo,
+      accessToken,
+      refreshToken,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Internal server error" });
+  }
+};
+
+const logout = async (req, res) => {
+  try {
+    // Do something
+    // xoa cookie don gian lam nguoc lai
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+    res.status(StatusCodes.OK).json({ message: "Logout API success!" });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+  }
+};
+
+const refreshToken = async (req, res) => {
+  try {
+    const refreshTokenFromBody = req.body?.refreshToken;
+    // verify refreshtoken co hop le khong
+    const refreshTokenDecoded = await JwtProvider.verifyToken(
+      //   accessTokenFromCookie,
+      refreshTokenFromBody,
+      env.REFRESH_TOKEN_SECRET_SIGNATURE
+    );
+    // lay thong tin tu decoded
+    const userInfo = {
+      id: refreshTokenDecoded.id,
+      email: refreshTokenDecoded.email,
+    };
+    // tao accesstoken moi
+    const accessToken = await JwtProvider.generateToken(
+      userInfo,
+      env.ACCESS_TOKEN_SECRET_SIGNATURE,
+      "1h"
+    );
+    // tra ve access token trong response de dinh vao localstorage
+    res.status(StatusCodes.OK).json({ accessToken });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Generate refresh token failed" });
+  }
+};
+export const authController = { refreshToken, logout, login };
---
+++ api/src/middlewares/authMiddleware.js
@@ -0,0 +1,50 @@
+import { StatusCodes } from "http-status-codes";
+import { env } from "../config/environment.js";
+import { JwtProvider } from "../providers/JwtProvider.js";
+
+// Xác thực và kiểm tra role
+const isAuthorized = (allowedRoles = []) => {
+  return async (req, res, next) => {
+    const accessTokenFromHeader = req.headers.authorization;
+
+    if (!accessTokenFromHeader) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "Unauthorized! (Token not found)" });
+    }
+
+    try {
+      // Giải mã token
+      const accessTokenDecoded = await JwtProvider.verifyToken(
+        accessTokenFromHeader.substring("Bearer ".length),
+        env.ACCESS_TOKEN_SECRET_SIGNATURE
+      );
+
+      // Gán giá trị đã giải mã vào req để xử lý phía sau
+      req.jwtDecoded = accessTokenDecoded;
+
+      // Kiểm tra role
+      const userRole = req.jwtDecoded.role;
+      if (!allowedRoles.includes(userRole)) {
+        return res
+          .status(StatusCodes.FORBIDDEN)
+          .json({ message: "Forbidden! (Access denied)" });
+      }
+
+      // Cho phép tiếp tục nếu hợp lệ
+      next();
+    } catch (error) {
+      console.log("Error from authMiddleware: ", error);
+      if (error.message?.includes("jwt expired")) {
+        return res
+          .status(StatusCodes.GONE)
+          .json({ message: "Need to refresh token" });
+      }
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "Unauthorized! (please login)" });
+    }
+  };
+};
+
+export const authMiddleware = { isAuthorized };
